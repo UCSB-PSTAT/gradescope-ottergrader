ce5ba286e52a696d9d52acaca0852872658bbdff
Sang-Yun Oh <syoh@ucsb.edu>
2021-03-16 16:22:05 -0700

    refine

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -13,79 +13,71 @@ gh auth login
 echo -en "\n## set git user information\n"
 printf "Enter a name for 'git config': "
 read GITHUB_NAME
-echo "GH NAME: ${GITHUB_NAME}"
 printf "Enter a email for 'git config': "
 read GITHUB_EMAIL
-echo "GH EMAIL: ${GITHUB_EMAIL}"
-
 
 git config --global user.email "${GITHUB_EMAIL}"
 git config --global user.name "${GITHUB_NAME}"
 
-echo -en "\n## create public repo on GitHub\n"
+echo -en "\n## create repositories GitHub\n"
 git init &>/dev/null
 gh repo create {{ cookiecutter.github_public_repo }} -y --public --description "{{ cookiecutter.class }} ({{ cookiecutter.term }})"
 rm -rf .git
 
-echo -en "\n## create private repo on GitHub\n"
 git init &>/dev/null 
 gh repo create {{ cookiecutter.github_private_repo }} -y --private --description "{{ cookiecutter.class }} ({{ cookiecutter.term }})"
 rm -rf .git
 
-echo -en "\n## create dual repostiory structure\n"
-echo -en "\ninitialize git repository\n"
+echo -en "\n## initialize git repository\n"
 git init
 
-echo -en "\nadd public/private reomotes\n"
+echo -en "\n# add public/private remotes\n"
 git remote add public-repo https://github.com/{{ cookiecutter.github_public_repo }}.git
 git remote add private-repo https://github.com/{{ cookiecutter.github_private_repo }}.git
+git remote -v
 
-echo -en "\ncreate/push public repository structure\n"
+echo -en "\n## create/push public repository\n"
 echo "# {{ cookiecutter.class }} ({{ cookiecutter.term }})" >> README.md
 git add README.md
 git commit -m "first commit"
 git branch -M public
 git push -u public-repo public
 
-echo -en "\ncreate/push private repository structure\n"
+echo -en "\n## create/push private repository\n"
 git checkout -b private
 
-echo -en "\n## GitHub and DockerHub tokens\n"
+echo -en "\n# GitHub and DockerHub tokens\n"
 
-stty -echo
-printf "GitHub personal access token is needed to access private repositories.\n"
-printf "Create one by following directions found here:\n"
-printf "https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token\n"
-printf "Enter GitHub Personal Access Token: "
+echo -en "\nGitHub personal access token allows access to private repositories.\n"
+echo -en "Instructions: https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token\n"
+echo -en "Enter GitHub Personal Access Token: "
 read MY_GITHUB_TOKEN
-printf "\n\n"
+echo -en "\n"
 
-printf "DockerHub access token is needed to upload Jupyter notebook docker images.\n"
-printf "Create one by following directions found here:\n"
-printf "https://docs.docker.com/docker-hub/access-tokens\n"
-printf "Enter dockerHub access token: "
+echo -en "\nDockerHub access token for uploading Jupyter notebook docker images.\n"
+echo -en "Instructions: https://docs.docker.com/docker-hub/access-tokens\n"
+echo -en "Enter your DockerHub access token: "
 read DOCKERHUB_ACCESS_TOKEN
-printf "\n\n"
+echo -en "\n"
 
-printf "DockerHub access token is needed to upload Gradescope grader docker images.\n"
-printf "Gradescope grader images are stored in a private repository\n"
-printf "Enter grader image repository access token: "
+echo -en "\nGradescope grader images are stored privately.\n"
+echo -en "Request a token from Sang-Yun Oh\n"
+echo -en "Enter access token for 'ucsbgrader' user: "
 read GRADER_ACCESS_TOKEN
-printf "\n\n"
-stty echo
+echo -en "\n"
 
-echo -en "\nadding tokens to GitHub secrets\n"
+echo -en "\n# adding tokens to GitHub secrets\n"
 gh secret set MY_GITHUB_TOKEN -b"${MY_GITHUB_TOKEN}"
 gh secret set DOCKER_USERNAME -b"{{ cookiecutter.dockerhub_username }}"
 gh secret set DOCKER_PASSWORD -b"${DOCKERHUB_ACCESS_TOKEN}"
 gh secret set GRADER_PASSWORD -b"${GRADER_ACCESS_TOKEN}"
 
-echo -en "\npushing grader files to private\n"
+echo -en "\n# pushing grader files to private\n"
 git add .
 git commit -m"initial grader files"
 git push -u private-repo private
 
-echo -en "\npushing again to docker builds notebook docker image)\n"
+echo -en "\n# pushing again to docker builds notebook docker image\n"
 git checkout -b docker
 git push -u private-repo docker
 
